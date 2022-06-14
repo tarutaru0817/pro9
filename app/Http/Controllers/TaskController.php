48812c9e844f0b5fca6d26969703beaf0c941238
tarutaru0817 <tarutaru.c@outlook.com>
2022-06-14 19:37:49 +0900

    チュートリアル7完了（真）

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -46,4 +46,33 @@ class TaskController extends Controller
             'id' => $current_folder->id,
         ]);
     }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 
 Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
