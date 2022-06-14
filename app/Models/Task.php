e984578f654813d39d334185311709c91a8b509f
tarutaru0817 <tarutaru.c@outlook.com>
2022-06-14 17:47:12 +0900

    チュートリアル５完了

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,60 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    /**
+    * 状態のラベル
+    * @return string
+    */
+    public function getStatusLabelAttribute()
+    {
+       // 状態値
+       $status = $this->attributes['status'];
+
+       // 定義されていなければ空文字を返す
+       if (!isset(self::STATUS[$status])) {
+           return '';
+       }
+
+       return self::STATUS[$status]['label'];
+    }
+
+     /**
+      *
+      * @return string
+      */
+     public function getStatusClassAttribute()
+     {
+
+         $status = $this->attributes['status'];
+
+         if (!isset(self::STATUS[$status])) {
+             return '';
+         }
+
+         return self::STATUS[$status]['class'];
+     }
+
+     /**
+     * 整形した期限日
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
+    }
 }
