40d412dbcf859275abd1c00445a2b8e6abe2061f
tarutaru0817 <tarutaru.c@outlook.com>
2022-06-14 19:03:44 +0900

    チュートリアル７完了

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->
 
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
